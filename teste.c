13c1393d8f5eb4a08510e66cb2bdfddf102de3b2
Allan <allancarneirodacunha@gmail.com>
2025-03-15 10:56:27 -0300

    implementando gitignore

--- teste.c
+++ teste.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
+int main(/*int argc, char const *argv[]*/)
 {
     /* code */
+    printf("hello world! \n");
     return 0;
 }
