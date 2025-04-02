dbc81a9cd62c06877263754f58c8aa7f6fe3b868
Allan <allancarneirodacunha@gmail.com>
2025-04-02 19:57:22 -0300

    começo da lista 3 e novo code snippet para C

---
+++ lista-de-exercicios-3/ex1.c
@@ -0,0 +1,25 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ 
+ /*
+ Nome: Allan Carneiro da Cunha Silveira
+ Data: 2025-04-02
+ Descrição: 1. Criando e exibindo um vetor. Crie um vetor de 5 posições e exiba seus valores.
+ */
+ 
+ int main(int argc, char const *argv[])
+ {
+    system("cls");
+    int vetor[5] = {1,2,3,4,5};
+
+    // printf("%d", sizeof(vetor)/ sizeof(int));
+
+    for (int i = 0; i < sizeof(vetor)/ sizeof(int) ; i++)
+    {
+        printf("vetor[%d] = %d\n",i, vetor[i]);
+    }
+    
+    
+    return 0;
+ }
+ 
---
+++ lista-de-exercicios-3/ex2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Nome: Allan Carneiro da Cunha Silveira
+Data: 2025-04-02
+Descrição: 2. Preenchendo um vetor com entrada do usuário. Peça ao usuário para preencher
+um vetor de 5 posições e exiba os valores digitados
+*/
+
+void main ()
+{
+    system("cls");
+    
+}
